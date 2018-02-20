aec48c7aa1a70b6da53ee4a60c9b589cb9ab8c60
GALILEO\knowter <ek@42rg.ru>
2018-02-20 11:04:57 +0300

    #1 Getting html page

--- src/main/java/ru/knowter/games/game100k1/GameApp.java
+++ src/main/java/ru/knowter/games/game100k1/GameApp.java
@@ -5,10 +5,27 @@
  */
 package ru.knowter.games.game100k1;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.Cookie;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  *
  * @author knowter
  */
 public class GameApp {
-  
+
+public static void main(String[] args) throws IOException {
+
+  try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_52, "10.250.25.59", 3128)) {
+    //webClient.setHTMLParserListener(HTMLParserListener.LOG_REPORTER);
+    final HtmlPage page = webClient.getPage("http://100-1.ru/");
+    String xmlPage = page.asXml();
+    System.out.println("xmlPage: \n"+xmlPage);
+  }
+}
 }
